d6f9e88fc8979e172e0d5f069578d4a8fb5779df
Pragatitrip <pragatitripathi117@gmail.com>
2023-12-19 09:31:18 +0530

    khjgsh

---
+++ index.js
@@ -0,0 +1,40 @@
+// let  i =4;
+// console.log("increment",i++)
+// console.log(i)
+
+// var inc=4;
+// var bmc=5;
+// console.log("increment",inc++)
+// console.log(inc)
+
+// let myname="pragati"
+// let bday=23
+// console.log("mybirthday is on",bday,myname)
+
+// let a=10
+// let b=20
+// let Result= a+b
+// console.log(" the sum of a a&b",Result)
+
+// let myname="pragati"
+// let S=4+5
+// console.log( `Mera nam ${myname} and my code is ${S}`)
+
+// let bday=23
+// let food="mattar paneer"
+// let place="vrindavan"
+// let hobby="travelling"
+// let animal="cow"
+// let pet=" dog"
+// let favcompany="tvf"
+// let branch="cse"
+// console.log( `my bday${bday} and my fav food${food} , place${plcae} , hobby${hobby} ,
+// animal${animal} , pet${pet} , favcompany${favcompany} , branch${branch} `)
+   
+
+for( let i=1; i<8 ; i++)
+{
+    var a=i+1;
+    console.log(`Sum of ${i} and ${a} = ${i+a} `  )
+}
+ 
